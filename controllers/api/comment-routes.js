45c0152b1cf991fc6225a5b0855994288960ac11
Zathras <ChampionMike42@gmail.com>
2021-08-14 19:43:09 -0500

    Add controllers

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,94 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// CREATE NEW COMMENT
+router.post("/", withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        })
+            .then((dbCommentData) => res.json(dbCommentData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
+
+// FIND ALL COMMENTS FOR ONE POST
+router.get("/:id", (req, res) => {
+    Comment.findAll({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// EDIT COMMENT
+router.put("/:id", withAuth, (req, res) => {
+    Comment.update(
+        {
+            comment_text: req.body.comment_text,
+        },
+        {
+            where: {
+                id: req.params.id,
+            },
+        }
+    )
+        .then((dbCommentData) => {
+            if (!dbCommentData) {
+                res.status(404).json({
+                    message: "No comment found with this id",
+                });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// DELETE COMMENT
+router.delete("/:id", withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((dbCommentData) => {
+            if (!dbCommentData) {
+                res.status(404).json({
+                    message: "No comment found with this id",
+                });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// FIND ALL COMMENTS
+router.get("/", (req, res) => {
+    Comment.findAll({})
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
